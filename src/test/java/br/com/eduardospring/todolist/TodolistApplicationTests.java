4ae651590ecd441d8fc3a07fa96777ac4870d308
eduardo <eduardobnhr@gmail.com>
2023-09-13 13:21:51 -0400

    first commit create project

---
+++ src/test/java/br/com/eduardospring/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardospring.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
